526b1b5b5d818b9a6cb08237fba9604bfc5b02d0
Luis Paolini <paoliniluis@gmail.com>
2021-02-07 17:26:30 -0300

    Fix a few horrors

--- run-reverse-proxies-with-metabase.sh
+++ run-reverse-proxies-with-metabase.sh
@@ -1,5 +1,3 @@
 #! /usr/bin/env bash
-
-docker-compose -f stacks/reverse-proxies/docker-compose.yml
-
-echo "running haproxy on localhost:8080, nginx on localhost:8081, envoy on localhost:8082" 
+cd stacks/reverse-proxies
+docker-compose up & echo "running haproxy on localhost:8080, nginx on localhost:8081, envoy on localhost:8082, metabase hidden behind, not exposed" 
