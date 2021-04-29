34773965fdb49267ac0c0850ccfb3d52581ea3a9
Cam Saul <github@camsaul.com>
2021-04-29 16:58:55 -0700

    Bump run-druid to Druid 0.20.2

--- run-druid.sh
+++ run-druid.sh
@@ -3,17 +3,19 @@
 SOURCE_DIR=`dirname "${BASH_SOURCE[0]}"`
 source "$SOURCE_DIR/common.sh"
 
-kill-existing druid-0-17-0
+kill-existing druid-0-20-2
 
 docker run \
        -p 8081:8081 \
        -p 8082:8082 \
        -p 8888:8888 \
        -e CLUSTER_SIZE=nano-quickstart \
-       --name druid-0-17-0 \
+       --name druid-0-20-2 \
        --rm \
-       -d metabase/druid:0.17.0
+       -d metabase/druid:0.20.2
 
-echo 'Started Druid on ports 8081, 8082, and 8888.'
+echo 'Started Druid 20.2 on ports 8081, 8082, and 8888.'
+echo
+echo 'Visit http://localhost:8081 in your browser to view the web console.'
 echo
 echo 'MB_DRUID_TEST_HOST=localhost MB_DRUID_TEST_PORT=8082'
