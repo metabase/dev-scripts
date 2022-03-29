7a841dd349c066bb183a25a20f7b0f21b39c2616
Bryan Maass <bryan.maass@gmail.com>
2022-03-29 13:55:35 -0600

    Adds script to run postgres + ssl locally (on port 5433)

---
+++ run_postgres_ssl/run_postgres_ssh.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euo pipefail
+
+openssl req -new -text -passout pass:abcd -subj /CN=localhost -out server.req -keyout privkey.pem
+
+openssl rsa -in privkey.pem -passin pass:abcd -out server.key
+
+openssl req -x509 -in server.req -text -key server.key -out server.crt
+
+chmod 600 server.key
+
+test $(uname -s) = Linux && chown 70 server.key
+
+echo '--------------------------------------------------'
+echo 'To Connect, Run:'
+echo ''
+echo 'psql -p 5433 "sslmode=verify-full host=localhost dbname=postgres user=postgres"'
+echo ''
+echo '--------------------------------------------------'
+
+PWD=$(pwd) docker-compose up --force-recreate
