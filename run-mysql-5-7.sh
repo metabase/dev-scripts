bbde31318b04bd8c45367a4faa25fa015ddb667a
Cam Saul <github@camsaul.com>
2022-07-29 11:38:32 -0700

    use cat <<EOF instead of a million echo statements

--- run-mysql-5-7.sh
+++ run-mysql-5-7.sh
@@ -10,10 +10,15 @@ docker run -p 3307:3306 \
        --rm \
        -d circleci/mysql:5.7.23
 
-echo "Started MySQL 5.7 on port 3307."
-echo
-echo "jdbc:mysql://localhost:3307/circle_test?user=root"
-echo
-echo "MB_DB_TYPE=mysql MB_DB_DBNAME=circle_test MB_DB_HOST=localhost MB_DB_PASS='' MB_DB_PORT=3307 MB_DB_USER=root MB_MYSQL_TEST_USER=root"
-echo
-echo "mysql --user=root --host=127.0.0.1 --port=3307 --database=circle_test"
+cat <<EOF
+Started MySQL 5.7 on port 3307.
+
+JDBC URL: jdbc:mysql://localhost:3307/circle_test?user=root
+
+env vars: MB_DB_TYPE=mysql MB_DB_DBNAME=circle_test MB_DB_HOST=localhost MB_DB_PASS='' MB_DB_PORT=3307 MB_DB_USER=root MB_MYSQL_TEST_USER=root"
+
+Connect with the MySQL CLI tool:
+
+mysql --user=root --host=127.0.0.1 --port=3307 --database=circle_test
+
+EOF
