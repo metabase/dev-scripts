2cfbbe4624e2083d8da010e65ab56aaba113e5c4
Cam Saul <github@camsaul.com>
2022-09-06 09:59:32 -0700

    Add notes about how to run MySQL 5.7 with Clojure CLI

--- run-mysql-5-7.sh
+++ run-mysql-5-7.sh
@@ -17,6 +17,21 @@ JDBC URL: jdbc:mysql://localhost:3307/circle_test?user=root
 
 env vars: MB_DB_TYPE=mysql MB_DB_DBNAME=circle_test MB_DB_HOST=localhost MB_DB_PASS='' MB_DB_PORT=3307 MB_DB_USER=root MB_MYSQL_TEST_USER=root"
 
+Clojure CLI:
+
+clj -J-Dmb.db-type=mysql -J-Dmb.db-port=3307 -J-Dmb.db.dbname=circle_test -J-Dmb.db.user=root -J-Dmb.db.pass=''
+
+or add a profile for it to your ~/.clojure/deps.edn:
+
+{:profiles
+ {:user/mysql-5-7
+  {:jvm-opts
+   ["-Dmb.db-type=mysql"
+    "-Dmb.db-port=3307"
+    "-Dmb.db.dbname=circle_test"
+    "-Dmb.db.user=root"
+    "-Dmb.db.pass="]}}}
+
 Connect with the MySQL CLI tool:
 
 mysql --user=root --host=127.0.0.1 --port=3307 --database=circle_test
