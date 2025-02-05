41b9210ad749a3ed2a5d57f7b1b1bfc0dc5e497b
Riley Thompson <rileythompson99@gmail.com>
2025-02-05 11:31:21 -0500

    fix redshift var

--- env-redshift.sh
+++ env-redshift.sh
@@ -13,10 +13,10 @@ USER=$(echo ${item_data} | jq -r '.fields[] | select(.label == "username").value
 function print-redshift-vars() {
     cat <<EOF
 Java properties:
--Dmb.redshift.test.access.key=${DB} -Dmb.redshift.test.host=${HOST} -Dmb.redshift.test.password=${PASSWORD} -Dmb.redshift.test.port=${PORT} -Dmb.redshift.test.user=${USER}
+-Dmb.redshift.test.db=${DB} -Dmb.redshift.test.host=${HOST} -Dmb.redshift.test.password=${PASSWORD} -Dmb.redshift.test.port=${PORT} -Dmb.redshift.test.user=${USER}
 
 Clojure pairs:
-:mb-redshift-test-access-key "${DB}" :mb-redshift-test-host "${HOST}" :mb-redshift-test-password "${PASSWORD}" :mb-redshift-test-port "${PORT}" :mb-redshift-test-user "${USER}"
+:mb-redshift-test-db "${DB}" :mb-redshift-test-host "${HOST}" :mb-redshift-test-password "${PASSWORD}" :mb-redshift-test-port "${PORT}" :mb-redshift-test-user "${USER}"
 
 Bash variables:
 MB_REDSHIFT_TEST_DB=${DB} MB_REDSHIFT_TEST_HOST=${HOST}  MB_REDSHIFT_TEST_PASSWORD=${PASSWORD} MB_REDSHIFT_TEST_PORT=${PORT}  MB_REDSHIFT_TEST_USER=${USER}
